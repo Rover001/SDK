44a5f80599daea57067644fdc0938d09dc3aaf11
Rover001 <zengyun6666@163.com>
2019-11-28 09:07:15 +0800

    测试swift静态包使用

---
+++ src/RWTestWork.framework/Headers/RWTestWork.h
@@ -0,0 +1,20 @@
+//
+//  RWTestWork.h
+//  RWTestWork
+//
+//  Created by linmao on 2019/11/28.
+//  Copyright © 2019 RoverWord. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RWTestWork.
+FOUNDATION_EXPORT double RWTestWorkVersionNumber;
+
+//! Project version string for RWTestWork.
+FOUNDATION_EXPORT const unsigned char RWTestWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWTestWork/PublicHeader.h>
+
+
+
